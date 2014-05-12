3eafbba84b04cfb0bcd9b3c8fa71fe83c88e8c3e
eugeneyan <eugeneyanziyou@gmail.com>
2014-05-12 15:24:34 +0800

    added start_stream() with auto restart loops

--- Tweets_to_json.py
+++ Tweets_to_json.py
@@ -3,39 +3,47 @@ import tweepy
 import json
 import os
 
-consumer_key=" "
-consumer_secret=" "
-access_key = " "
-access_secret = " "
+consumer_key= ""
+consumer_secret= ""
+access_key = ""
+access_secret = ""
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 # directory that you want to save the json file
-# os.chdir("C:\Users\IBM_ADMIN\Desktop\json_files")
+# os.chdir("C:\Users\Desktop\json_files")
 # name of json file you want to create/open and append json to
-save_file = open("11may.json", 'a')
+save_file = open("12may.json", 'a')
 
 class CustomStreamListener(tweepy.StreamListener):
     def __init__(self, api):
         self.api = api
         super(tweepy.StreamListener, self).__init__()
 
-        self.list_of_tweets = []
-
+        # self.list_of_tweets = []
+        
     def on_data(self, tweet):
-        # self.list_of_tweets.append(json.loads(tweet))
-        print tweet
+        print tweet 
         save_file.write(str(tweet))
 
     def on_error(self, status_code):
         print >> sys.stderr, 'Encountered error with status code:', status_code
         return True # Don't kill the stream
+        print "Stream restarted"
 
     def on_timeout(self):
         print >> sys.stderr, 'Timeout...'
         return True # Don't kill the stream
+        print "Stream restarted"
 
-sapi = tweepy.streaming.Stream(auth, CustomStreamListener(api))
-sapi.filter(track=["Sony", "Xperia", "iPhone", "Apple", "Samsung", "s4", "s5", "note" "3", "HTC", "Blackberry", "q5", "q10", "z10", "Nokia", "Lumia", "Nexus", "LG", "Huawei", "Motorola"])
+def start_stream():
+    while True:
+        try:
+            sapi = tweepy.streaming.Stream(auth, CustomStreamListener(api))
+            sapi.filter(track=["Samsung", "s4", "s5", "note" "3", "HTC", "Sony", "Xperia", "Blackberry", "q5", "q10", "z10", "Nokia", "Lumia", "Nexus", "LG", "Huawei", "Motorola"])
+        except: 
+            continue
+
+start_stream()
 
