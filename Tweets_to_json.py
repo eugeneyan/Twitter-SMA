e8e02325f59928a625c858cdbb8a68e226477f82
eugeneyan <eugeneyanziyou@gmail.com>
2014-05-15 20:52:24 +0800

    no message

--- Tweets_to_json.py
+++ Tweets_to_json.py
@@ -4,16 +4,16 @@ import json
 import os
 import logging
 
-consumer_key = ""
-consumer_secret = ""
-access_key = ""
-access_secret = ""
+consumer_key="Uyk7A893bypMmcpdWMSvB6VbZ"
+consumer_secret="2Ml8ZkhmGllbeVSu98KnHtVpNmHCIXGaKQObVTXtBwwwDPmOHU"
+access_key = "35109534-sPwWPITGOsKkFTKPhOwXqiRkWAb55QWgNrBtWURiC"
+access_secret = "8AZWXWYDO1qaCSc2lltxGOPZV3OKpMdY5PzevBj9n5lpm"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 # directory that you want to save the json file
-os.chdir("C:\Users\IBM_ADMIN\Desktop\json_files")
+# os.chdir("C:\Users\IBM_ADMIN\Desktop\json_files")
 # name of json file you want to create/open and append json to
 save_file = open("15may.json", 'a')
 
